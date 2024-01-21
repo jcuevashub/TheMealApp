47345ece5941a1c56a549056553b31797c6cc6e0
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-01-21 16:18:27 -0400

    bug fixed

---
+++ Catalog.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Infrastructure;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+}
---
+++ Email.Shared/ServiceExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Email.Shared;
+
+public static class ServiceExtension
+{
+    public static void AddSharedInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        //services.AddTransient<ITheMealService, TheMealService>();
+    }
+
+}
